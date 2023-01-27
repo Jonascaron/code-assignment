f1b3a1c07c401b82e62e8b1034bb801c80e9df9e
Jonas Caron <jonas.caron16@gmail.com>
2023-01-27 15:32:20 +0100

    opdacht af

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 /** Voeg hier je code toe */
+const ADD = document.getElementById('add');
+const ASSIGNMENT_03 = document.getElementById('assignment-03');
+console.log(ADD);
+let i = 0;
+
+ADD.addEventListener('click',function(){
+    if (i < 10) {
+        let creatediv = document.createElement('div');
+        i++;
+        creatediv.innerText = i;
+        creatediv.setAttribute('class','block');
+        ASSIGNMENT_03.appendChild(creatediv); 
+    }
+    else{
+        alert('er mogen niet meer dan 10 blokken gemaakt worder')
+    }
+
+})
